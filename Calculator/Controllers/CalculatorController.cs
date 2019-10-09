bda5c80ba793b425104db2bc08bfce7b2d3a409a
Alexander Lysenko <alexander.lysenko1@gmail.com>
2019-10-09 12:34:32 +0300

    Add project files.

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,78 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Evaluate(float first,float second,string oper)
+        {
+            double result = 0;
+            Expression expression = new Expression();
+           
+            switch (oper)
+            {
+                case "+":
+                    result = first + second;
+                    expression.Operation = first.ToString() + oper.ToString() + second.ToString();
+                    break;
+                case "-":
+                    result = first - second;
+                    expression.Operation = first.ToString() + oper.ToString() + second.ToString();
+                    break;
+                case "÷":
+                    result = first / second;
+                    expression.Operation = first.ToString() + oper.ToString() + second.ToString();
+                    break;
+                case "×":
+                    result = first * second;
+                    expression.Operation = first.ToString() + oper.ToString() + second.ToString();
+                    break;
+                case "√":
+                    result = Math.Sqrt(second);
+                    expression.Operation = oper.ToString() + second.ToString();
+                    break;
+                case "%":
+                    result = second / 100;
+                    expression.Operation = oper.ToString() + second.ToString();
+                    break;
+                case "х²":
+                    result = second * second;
+                    expression.Operation = oper.ToString() + second.ToString();
+                    break;
+                case "lx":
+                    result = 1 / second;
+                    expression.Operation = oper.ToString() + second.ToString();
+                    break;
+                case "±":
+                    result = -1 * second;
+                    expression.Operation = oper.ToString() + second.ToString();
+                    break;
+
+            }
+            expression.Result = result.ToString();
+            db.Expressions.Add(expression);
+            db.SaveChanges();
+            return expression.Result;
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Expressions()
+        {
+            var db = new ApplicationDbContext();
+            return Json(db.Expressions, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Calculator/Scripts/Calculator/Calculator.js
@@ -0,0 +1,71 @@
+﻿function Eval(op1, op2, oldOp, newOp) {
+    $.ajax({
+        method: "Post",
+        url: "/Calculator/Evaluate",
+        data:{ first: op1, second: op2, oper: oldOp },
+        success: function (result) {
+            $("#op1").val(result);
+            $("#op2").val("");
+            $("#oldOperator").val(newOp);
+            $("#newOperator").val("");
+            $("#result").val(result + newOp);
+            getAllExpressions();
+        },
+        error: function (error) {
+            alert(error);
+        }
+
+    })
+}
+
+$(document).ready(function () {
+    $(".keypad").click(function (e) {
+        var value = e.currentTarget.innerHTML;
+        var oldOp = $('#oldOperator').val();
+        var newOp = $('#newOperator').val();
+        var op1 = $("#op1").val();
+        if ($.trim(op1) == "" && (oldOp == "√" || oldOp == "%" || oldOp == "х²" || oldOp == "lx" || oldOp == "±")) {
+            op1 = 1;
+        }
+        var op2 = $("#op2").val();
+        if (value != "=") {
+            var result = $("#result").val();
+            result = result + value;
+            var operators = ["+", "-", "×", "÷", "√", "%", "х²", "lx","±","CE"];
+            if ($.inArray(value, operators) != -1) {
+                
+                if ($.trim(op1) == "" && (oldOp == "+" || oldOp == "-" || oldOp == "×" || oldOp == "÷")) { 
+                    alert("s");
+                    return false;
+                }
+                if ($.trim(oldOp) == "") {
+                    $("#oldOperator").val(value);
+                }
+                else {
+                    if ($.trim(newOp) == "" || newOp == undefined) {
+                        if ($.trim(op2) == "") {
+                            alert("s");
+                            return false;
+                        }
+                        else {
+                            $("#newOperator").val(value);
+                            Eval(op1, op2, oldOp, value);
+                        }
+                    }
+                }
+            }
+            else {
+                if (oldOp == undefined || oldOp == "") {
+                    $("#op1").val(op1 + value);
+                }
+                else {
+                    $("#op2").val(op2 + value);
+                }
+            }
+            $("#result").val(result);
+        }
+        else {
+            Eval(op1, op2, oldOp, newOp);
+        }
+    });
+});
